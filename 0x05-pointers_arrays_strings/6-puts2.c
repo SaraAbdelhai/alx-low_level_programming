d0b35e0d3125355d405c47efce21236391b833c9
SaraAbdelhai <sara.h.abdalhy@gmail.com>
2023-10-16 15:52:23 -0700

    a

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts2 - prints every character of a string
+ * puts2 - this will print all even of a string
  *
  * @str: string paramater input
  *
@@ -14,8 +14,12 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != "\0"; ++i)
 	{
+
 		if (i % 2 == 0)
+		{
 			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
+
 }
