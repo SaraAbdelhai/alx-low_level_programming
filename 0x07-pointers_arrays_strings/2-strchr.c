92840f14bcaf66188b9a5c32510375e68801c887
SaraAbdelhai <sara.h.abdalhy@gmail.com>
2023-10-23 16:09:59 -0700

    a

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,21 +3,24 @@
 /**
  * *_strchr - locates a character in a string.
  * @s: string
- * @c: constant
- * Return: pointer to s
+ * @c: char
+ * Return: pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found.
  */
-
 char *_strchr(char *s, char c)
 {
-	int i;
+		int a;
 
-	for (i = 0; s[i] >= '\p' ; i++)
-	{
-		if (s[i] == c)
+		while (1)
 		{
-			return (s + i);
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
 		}
-	}
-
-	return ('\0');
 }
